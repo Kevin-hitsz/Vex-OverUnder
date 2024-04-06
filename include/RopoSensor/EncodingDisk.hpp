9b1b8aa6062f053cb490c73e716b98afb57d371c
lin-yihangtfl <1784543599@qq.com>
2024-04-06 10:33:56 +0800

    码盘

--- include/RopoSensor/EncodingDisk.hpp
+++ include/RopoSensor/EncodingDisk.hpp
@@ -73,19 +73,19 @@ namespace RopoSensor{
 			//x为机体正前方，y为机体正左
 			//获得码盘相对于机体零点机体x方向的坐标pos_x
 			float GetEncodingPosX0(){
-				return  PosY * RopoMath::Cos(Encoding_in_Car_Angle) + PosX * RopoMath::Sin(Encoding_in_Car_Angle) + Encoding_in_Car_X;
+				return   PosX * RopoMath::Cos(Encoding_in_Car_Angle) - PosY * RopoMath::Sin(Encoding_in_Car_Angle);
 			}		
 			//获得码盘相对于机体零点机体y方向的坐标pos_y
 			float GetEncodingPosY0(){
-				return -PosX * RopoMath::Cos(Encoding_in_Car_Angle) + PosY * RopoMath::Sin(Encoding_in_Car_Angle) + Encoding_in_Car_Y;
+				return  PosY * RopoMath::Cos(Encoding_in_Car_Angle) + PosX * RopoMath::Sin(Encoding_in_Car_Angle);
 			}			
 			//获得机体相对于零点x方向的坐标pos_x
 			float GetPosX(){
-				return  GetEncodingPosX0() - Encoding_in_Car_X * RopoMath::Cos(Angle[2]) + Encoding_in_Car_Y * RopoMath::Cos(Angle[2]);
+				return   GetEncodingPosY0() - Encoding_in_Car_Y * RopoMath::Cos(Angle[2]) - Encoding_in_Car_X * RopoMath::Cos(Angle[2])+ Encoding_in_Car_Y;
 			}		
 			//获得机体相对于零点y方向的坐标pos_y
 			float GetPosY(){
-				return  GetEncodingPosY0() - Encoding_in_Car_Y * RopoMath::Cos(Angle[2]) - Encoding_in_Car_X * RopoMath::Cos(Angle[2]);
+				return   -(GetEncodingPosX0() - Encoding_in_Car_X * RopoMath::Cos(Angle[2]) + Encoding_in_Car_Y * RopoMath::Cos(Angle[2])+ Encoding_in_Car_X);
 			}		
 			//获得机体偏离Tag所示轴向的角度Tag angle 
 				//Tag: 0_x 1_y 2_z
