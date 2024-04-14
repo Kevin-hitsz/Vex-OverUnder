3f8a6cafc11f2236ae5a880e3f4c9abc5a050238
tyj <BXQSQaQ@outlook.com>
2024-04-15 02:28:31 +0800

    更新底盘自动参数

--- include/RopoChassis.hpp
+++ include/RopoChassis.hpp
@@ -24,7 +24,7 @@ namespace RopoChassis{
 			static constexpr float DeltaVelocity_in_AccelerationProcess = 0.004;  //加速过程每SampleTime的增加的速度	0.0025
 			static constexpr float AccelerationVelocityLimits = 1.1;
 			//控制器参数为p，i，d，最大值限幅，最小值限幅，误差容限，到达退出时间（秒）
-			inline static RopoControl::PIDRegulator DistanceRegulator{0.0026 ,0.0006  ,0.00003 ,0.0014,-0.0014,0.02,0.3};
+			inline static RopoControl::PIDRegulator DistanceRegulator{0.0026 ,0.0002  ,0.00006 ,0.0014,-0.0014,0.02,0.3};
 			//0.0026 ,0.0001  ,0.00001 ,0.00075,-0.00075,0.02,0.3
 			inline static RopoControl::PIDRegulator SlowDegRegulator {0.00007,0.000003,0.000001,0.0030 ,-0.0030 ,3   ,0.2};
 			//0.00007,0.000003,0.000001,0.0030 ,-0.0030 ,3   ,0.2
